a346cdc70deeda4e36fea8544e5271df3d876178
eonblu <yannis.klingele@gmail.com>
2023-11-09 20:54:01 +0000

    Added generation text, added second slider that currently doesnt do anything

--- src/D3.js
+++ src/D3.js
@@ -9,8 +9,13 @@ const NetworkVisualization = () => {
   const width = window.innerWidth * 0.8;
   const height = window.innerHeight * 0.8;
   var [randomChance, setRandomChance] = useState(0.7);
+  var [randomChance2, setRandomChance2] = useState(0.7);
+  var generationvalue = 0;
+  let generationtext;
 
-  const findNeighboringNodes = useCallback((clickedNodeId, newColor, svg, links) => {
+  const findNeighboringNodes = useCallback((clickedNodeId, newColor, svg, links, generation) => {
+    // eslint-disable-next-line
+    if (generationvalue < generation) {generationvalue = generation; generationtext.text('Generation: ' +  generation)};
     const connectedLinks = links.filter(
       (link) => link.source.id === clickedNodeId || link.target.id === clickedNodeId
     );
@@ -24,7 +29,7 @@ const NetworkVisualization = () => {
         if (!targetNode.empty() && targetNode.attr('fill') !== newColor && Math.random() < randomChance) {
           setTimeout(() => {
             targetNode.attr('fill', newColor);
-            findNeighboringNodes(link.target.id, newColor, svg, links);
+            findNeighboringNodes(link.target.id, newColor, svg, links, generation + 1);
           }, 1000); // Change this value to the desired delay time in milliseconds
         }
       } else {
@@ -35,12 +40,12 @@ const NetworkVisualization = () => {
         if (!sourceNode.empty() && sourceNode.attr('fill') !== newColor && Math.random() < randomChance) {
           setTimeout(() => {
             sourceNode.attr('fill', newColor);
-            findNeighboringNodes(link.source.id, newColor, svg, links);
+            findNeighboringNodes(link.source.id, newColor, svg, links, generation + 1);
           }, 1000); // Change this value to the desired delay time in milliseconds
         }
       }
     });
-  }, [randomChance]);
+  }, [randomChance, generationvalue, generationtext]);
 
 useEffect(() => {
   const generateRandomLinks = (nodeCount, linkCount, nodes) => {
@@ -66,6 +71,8 @@ useEffect(() => {
     let drawArea = svg.select('.drawArea');
 
     const resetGraph = (buttonID) => {
+      // eslint-disable-next-line
+      generationvalue = 0; generationtext.text('Generation: ' +  generationvalue);
       if (buttonID === 1) {nodes = data.nodes1; links = data.links1}
       else if (buttonID === 2) {nodes = data.nodes2; links = data.links2}
       else if (buttonID === 3) {nodes = data.nodes3; links = data.links3};
@@ -104,7 +111,9 @@ useEffect(() => {
         const handleRandomChanceChange = (event) => {
           const newRandomChance = parseFloat(event.target.value);
           setRandomChance(newRandomChance);
+          // eslint-disable-next-line
           randomChance = newRandomChance;
+          sliderText.innerHTML = 'Value ' + randomChance;
         };
 
         const slider = document.createElement('input');
@@ -116,6 +125,32 @@ useEffect(() => {
         slider.classList.add('slider');
         slider.addEventListener('input', handleRandomChanceChange);
         buttonWrapper.appendChild(slider);
+
+        var sliderText = document.createElement('sliderText');
+        sliderText.innerText = 'Value ' + randomChance;
+        buttonWrapper.appendChild(sliderText);
+
+        const handleRandomChanceChange2 = (event) => {
+          const newRandomChance2 = parseFloat(event.target.value);
+          setRandomChance2(newRandomChance2);
+          // eslint-disable-next-line
+          randomChance2 = newRandomChance2;
+          sliderText2.innerHTML = 'Value ' + randomChance2;
+        };
+
+        const slider2 = document.createElement('input');
+        slider2.type = 'range';
+        slider2.min = 0;
+        slider2.max = 1;
+        slider2.step = 0.01;
+        slider2.value = randomChance;
+        slider2.classList.add('slider');
+        slider2.addEventListener('input', handleRandomChanceChange2);
+        buttonWrapper.appendChild(slider2);
+
+        var sliderText2 = document.createElement('sliderText');
+        sliderText2.innerText = 'Value ' + randomChance;
+        buttonWrapper.appendChild(sliderText2);         
         };
 
     // Create a zoom behavior
@@ -129,7 +164,18 @@ useEffect(() => {
 
     // Add zoom behavior to SVG
     svg.call(zoom);
-    
+
+    // Add text to the SVG area
+    // eslint-disable-next-line
+    generationtext = svg
+    .append('text')
+    .attr('id', 'generationtext')
+    .attr('x', 10) // Adjust the x position as needed
+    .attr('y', height - 10) // Adjust the y position as needed
+    .style('font-size', '20px') // Adjust the font size as needed
+    .style('fill', 'black') // Adjust the text color as needed
+    .text('Generation: ' + generationvalue); // Add your desired text here
+        
     // Create a simple graph
     const simulation = d3
       .forceSimulation(nodes)
@@ -182,7 +228,7 @@ useEffect(() => {
         clickedNode.attr('fill', newColor);
 
         // Find the neighboring nodes and change their colors immediately
-        findNeighboringNodes(clickedNodeId, newColor, svg, links);
+        findNeighboringNodes(clickedNodeId, newColor, svg, links, 1);
       });
 
 //      node
@@ -211,7 +257,7 @@ useEffect(() => {
 
 createGraph();
 
-}, [width, height, findNeighboringNodes, randomChance]);
+}, [width, height, findNeighboringNodes, randomChance, generationvalue, generationtext]);
 
 return (
   <div style={{ display: 'flex' }} ref={wrapperRef}>
